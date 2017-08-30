76025ddc09bcd1a6feb3c90e6a282738a1515a3e
Karen Anewalt <karenanewalt@TRIN-B19-KA2.local>
2017-08-29 21:55:13 -0400

    This is the second commit; added a comment.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        //This is a test comment.  I'm practicing with Git.
         System.out.println("Practice Project");
     }
 }
